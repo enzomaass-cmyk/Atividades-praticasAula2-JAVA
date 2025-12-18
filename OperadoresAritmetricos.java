0a4cdfa9289e65a29c8ce047d2f13e101820b13f
enzo maass <enzo_maass@estudante.sesisenai.org.br>
2025-12-18 20:53:31 -0300

    exercicio2 pronto

---
+++ OperadoresAritmetricos.java
@@ -0,0 +1,21 @@
+public class OperadoresAritmetricos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        // Adição
+        System.out.println("Adição: " + (a + b));
+
+        // Subtração
+        System.out.println("Subtração: " + (a - b));
+
+        // Multiplicação
+        System.out.println("Multiplicação: " + (a * b));
+
+        // Divisão
+        System.out.println("Divisão: " + (a / b));
+
+        // Módulo
+        System.out.println("Módulo: " + (a % b));
+    }
+}
