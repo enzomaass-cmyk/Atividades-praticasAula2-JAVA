babbea9b4a075444d02517b926bd7ded98fc68b1
enzo maass <enzo_maass@estudante.sesisenai.org.br>
2025-12-18 20:56:30 -0300

    exercicio3 pronto

---
+++ OperadoresdeComparecao.java
@@ -0,0 +1,12 @@
+public class OperadoresdeComparecao {
+    public static void main(String[] args) {
+        int idade = 25;
+
+        System.out.println("idade == 25: " + (idade == 25));
+        System.out.println("idade != 30: " + (idade != 30));
+        System.out.println("idade > 18: " + (idade > 18));
+        System.out.println("idade < 30: " + (idade < 30));
+        System.out.println("idade >= 25: " + (idade >= 25));
+        System.out.println("idade <= 25: " + (idade <= 25));
+    }
+}
