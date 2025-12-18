da9eed84e760c19b1fe5f66410f716b7d125d330
enzo maass <enzo_maass@estudante.sesisenai.org.br>
2025-12-18 20:48:53 -0300

    exercicio1 pronto

---
+++ DeclaracaodeVariaveis.java
@@ -0,0 +1,18 @@
+public class DeclaracaodeVariaveis {
+    public static void main(String[] args) {
+
+        // Declaração e inicialização das variáveis
+        int idade = 25;
+        double altura = 1.75;
+        String nome = "João Silva";
+        boolean ativo = true;
+        char inicial = 'J';
+
+        // Exibição dos valores
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nome: " + nome);
+        System.out.println("Ativo: " + ativo);
+        System.out.println("Inicial: " + inicial);
+    }
+}
